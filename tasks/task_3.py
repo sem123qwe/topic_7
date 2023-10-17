9cf92444af196ad4ba199130b7f45e1528d2f0e6
Sem Salimov <salimovsemen43@gmail.com>
2023-10-17 22:03:09 +0600

    7. Циклы
    * Tasks: 0, 1, 2, 3, 4, 5

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -1,1 +1,9 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+print("made thought for:")
+for num in range (2, 21, 2):
+    print(num)
+print("made thought whill")
+num = 2
+
+while num <= 20:
+    print(num)
+    num += 2
--- tasks/task_4.py
+++ tasks/task_4.py
@@ -1,1 +1,7 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+num = 0
+n = 1
+user_input = int(input(""))
+while n <= user_input:
+    num += n
+    print(num)
+    n += 1    
