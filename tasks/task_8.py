3e246e790efc1088eba32bda1ee177d7af8fb451
Sem Salimov <salimovsemen43@gmail.com>
2023-10-27 22:13:52 +0600

    7. Циклы
    *task 8

--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,7 +1,5 @@
 user_input_start = int(input("Введите начало диапазона: "))
 user_input_finish = int(input("Введите конец диапазона: "))
-if user_input_start == (user_input_finish % 2 == 0):
-    print(user_input_start)
-elif user_input_start == user_input_finish:
-    print(0)
+for num in range(user_input_start, user_input_finish + 1, 2):
+    print(num)
     
